873bce8cdcd12696ae74ba6173a2c51e648b296f
msalamonski <msalamonski@dismail.de>
2020-12-01 13:59:00 -0500

    ⭐️⭐️ finished second challenge

--- 1/cat.py
+++ 1/cat.py
@@ -5,12 +5,12 @@
 '''
 with open('input.txt', 'r') as i:
     INP = i.readlines()
-    INP = [x.strip() for x in INP]
+    INP = [x.strip() for x in INP] # take out \n from readlines()
 
 def cat():
     ''' find two entries that sum to 2020, and multiply them '''
     for x in INP:
-        x = int(x)
+        x = int(x) # x is read as a string from readlines(). Convert to int.
         for y in INP:
             y = int(y)
             print(str(x) + ' + ' + str(y) + ' == ' + str(x + y))
@@ -18,5 +18,19 @@ def cat():
                 return 'final answer is: ' + str(x * y)
     return 'not found'
 
+def threecat():
+    ''' find three entries that sum to 2020, and multiply them '''
+    for x in INP:
+        x = int(x)
+        for y in INP:
+            y = int(y)
+            for z in INP:
+                z = int(z)
+                print(str(x) + ' + ' + str(y) + ' + ' + str(z) + ' == ' + str(x + y + z))
+                if x + y + z == 2020:
+                    return 'final answer is: ' + str(x * y * z)
+    return 'not found'
+
 if __name__ == '__main__':
     print(cat())
+    # print(threecat())
