e7fb60573a5538ec31e7e7ced2fa220b242b4d2e
msalamonski <msalamonski@dismail.de>
2020-12-02 01:14:14 -0500

    ⭐️ finished part 1 of day 2 challenge

---
+++ 2/policy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' AoC 2 '''
+with open('input.txt', 'r') as i:
+    INP = i.readlines()
+    INP = [x.strip() for x in INP] # take out \n from readlines()
+
+def parse():
+    ''' parse input lines, and validate if password meets policy'''
+    valid = 0 # number of valid passwords
+    for line in INP:
+        par = line.split(' ')
+        freq = par[0].split('-') # frequency
+        lett = par[1][0] # letter/character
+        pasw = par[2] # password
+
+        occ = occurances(lett, pasw)
+        if int(freq[0]) <= occ <= int(freq[1]):
+            valid += 1
+    return valid
+
+def occurances(lett, pasw):
+    ''' find occurances of a letter in a password '''
+    occ = 0
+    for char in list(pasw): # for each character in the password
+        if char == lett: # if the selected character matches the letter
+            occ += 1
+    return occ
+
+if __name__ == '__main__':
+    print(parse())
+    # parse()
