6739761dd956a4a9c64f2a0e37f1f92252b490ee
msalamonski <msalamonski@dismail.de>
2020-12-03 02:11:31 -0500

    ⭐️⭐️ finished day 3

--- 3/toboggan.py
+++ 3/toboggan.py
@@ -4,22 +4,25 @@ with open('input.txt', 'r') as i:
     INP = i.readlines()
     INP = [x.strip() for x in INP] # take out \n from readlines()
 
-def traverse():
+def traverse(x, y):
     ''' see how many trees encountered '''
     cur = [0, 0] # x, y # cursor
     tree = 0
     for line in INP:
+        if y == 2 and ((cur[1] % 2) != 0):
+            continue
         if cur[1] == 0: # if you're on the first line
-            cur[0] += 3 # right 3
-            cur[1] += 1 # down 1
+            cur[0] += x # right x amount of times
+            cur[1] += y # down y
             continue
         if cur[0] >= len(line): # go back to left side if touching right
             cur[0] = cur[0] - len(line)
         if '#' in line[cur[0]]:
-            tree +=1
-        cur[0] += 3
-        cur[1] += 1
+            tree += 1
+        cur[0] += x
+        cur[1] += y
+        print('x: ' + str(cur[0]) + ' y: ' + str(cur[1]))
     return tree
 
 if __name__ == '__main__':
-    print(traverse())
+    print(traverse(1, 1))
