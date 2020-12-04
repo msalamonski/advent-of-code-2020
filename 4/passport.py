f30799d716dfdd2fb1642ed82b8bb762a4541b5a
msalamonski <msalamonski@dismail.de>
2020-12-04 17:39:56 -0500

    ⚙️ cleanup

--- 4/passport.py
+++ 4/passport.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-''' AoC 4 -- rough draft -- part 2 not yet working'''
-import re
+''' AoC 4 '''
+from re import search
 
 with open('input.txt', 'r') as i:
     INP = i.readlines()
@@ -8,35 +8,29 @@ with open('input.txt', 'r') as i:
 
 def passport():
     ''' count number of passports with 7 key:values present '''
-    valid = 0
-    new_valid = 0
-    passp = ''
-    based = 0
-    for passport in INP:
-        if len(passport) > 3:
-            passp += (' ' + passport)
+    valid, new_valid, passp = 0, 0, ''
+    key = 'ecl', 'pid', 'eyr', 'hcl', 'byr', 'iyr', 'hgt'
+    for passpor in INP:
+        if len(passpor) > 3:
+            passp += (' ' + passpor)
             continue
-        if ('ecl' in passp and
-            'pid' in passp and
-            'eyr' in passp and
-            'hcl' in passp and
-            'byr' in passp and
-            'iyr' in passp and
-            'hgt' in passp):
-            valid += 1
-            based += 1
-            new_valid += valid_passport(passp)
+        check = 0
+        for k in key:
+            if k in passp:
+                check += 1
+                if check == 7:
+                    valid += 1 # for part 1
+                    new_valid += valid_passport(passp) # for part 2
         passp = ''
-    return 'valid passports: ' + str(new_valid) # note that the last line is not counted
+    return 'valid passports: ' + str(new_valid)
 
 def valid_passport(line):
     ''' count the number of passports with values present and valid '''
     key = 'ecl', 'pid', 'eyr', 'hcl', 'byr', 'iyr', 'hgt'
     color = 'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'
     ecl, pid, eyr, hcl, byr, iyr, hgt = '', '', '', '', '', '', ''
-    valid = 0
-    passp = line.split()
-    for k in passp:
+    valid, passp = 0, line.split()
+    for k in passp: # assign values in a passport to it's respective key
         if key[0] in k:
             ecl = k[4:]
         if key[1] in k:
@@ -51,30 +45,27 @@ def valid_passport(line):
             iyr = k[4:]
         if key[6] in k:
             hgt = k[4:]
-
+    # begin validation
     if ((1920 <= int(byr) <= 2002) and
     (2010 <= int(iyr) <= 2020) and
     (2020 <= int(eyr) <= 2030)):
-        print(byr)
-        if 'cm' in hgt:
-            if 150 <= int(hgt.replace('cm', '')) <= 193:
-                match = re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', hcl)
-                if match:
-                    for eye in color:
-                        if eye in ecl:
-                            if len(pid) == 9:
-                                valid += 1
-        if 'in' in hgt:
-            if 59 <= int(hgt.replace('in', '')) <= 76:
-                match = re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', hcl)
-                if match:
-                    for eye in color:
-                        if eye in ecl:
-                            if len(pid) == 9:
-                                valid += 1
-
+        mes = validate(hgt)
+        if mes[0] <= int(hgt.replace(mes[1], '')) <= mes[2]:
+            if search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', hcl):
+                for eye in color:
+                    if eye in ecl:
+                        if len(pid) == 9:
+                            valid += 1
     return valid
 
+def validate(hgt):
+    ''' validate height -- check if 'cm' or 'in' in 'hgt' '''
+    mes = 1, '', -1 # cause an entry with missing 'cm' or 'in' to be false
+    if 'cm' in hgt:
+        mes = 150, 'cm', 193
+    if 'in' in hgt:
+        mes = 59, 'in', 76
+    return mes
 
 if __name__ == '__main__':
-    print(passport())
+    print(passport()) # note that the last line in the input is not counted
