2139fb3caa31cfdf386b2b3507491e13788c247f
msalamonski <msalamonski@dismail.de>
2020-12-08 23:35:49 -0500

    small edits

--- 8/accu.py
+++ 8/accu.py
@@ -24,5 +24,25 @@ def accumulator():
         step = '!'
     return None
 
+def fix_acc():
+    ''' fix the accumulator (part 2) (incomplete) '''
+    # generate lists
+    inst = []
+    x = []
+    skip = 0
+    for line in fileinput.input():
+        inst.append(line.strip())
+    # make a list of lists. if val in item, replace. else, return item.
+    for i in range(0, len(inst)):
+        if 'jmp' in inst[i]:
+            x.append(inst[i].replace('jmp', 'nop'))
+            break
+    # make each list in the list change only one item. increment skip, and break
+    print(inst)
+    print([x for i in x]) # list comprehension
+
+    return None
+
 if __name__ == '__main__':
-    print(accumulator())
+    print(accumulator()) # part 1
+    # print(fix_acc()) # part 2
