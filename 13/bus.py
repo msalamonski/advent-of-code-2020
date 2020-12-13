40ec9415e2d46cfb24734298599653ad10a48fe3
msalamonski <msalamonski@dismail.de>
2020-12-13 09:42:11 -0500

    ⭐️ finished part 1 (started late)

---
+++ 13/bus.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' AoC 13 -- ./bus.py 13.ex '''
+import fileinput
+import copy
+
+def bus(): # 1000508 # 1000517 - 1000508 = 9 # 37*9 = 333
+    ''' What is the ID of the earliest bus... '''
+    i, k, timestamp, busid = 0, 0, 0, 0
+    based = []
+    for line in fileinput.input():
+        if i == 0:
+            timestamp = int(line.strip())
+        else:
+            busid = line.replace('x,', '').strip().split(',')
+        i += 1
+    newbusid = copy.deepcopy(busid)
+    i = 0
+    for i in range(len(busid)):
+        while int(newbusid[i]) < timestamp:
+            newbusid[i], busid[i] = int(newbusid[i]), int(busid[i])
+            newbusid[i] += busid[i]
+
+    return newbusid
+
+if __name__ == '__main__':
+    print(bus())
+
